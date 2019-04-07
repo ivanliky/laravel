1a8d06dd4cb5d0e837ce03a348aa26c8a55e4b64
Ivan Mitic <ivanmitic91@gmail.com>
2019-04-07 11:47:54 +0200

    Working all

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -20,3 +20,8 @@ Route::auth();
 Route::get('/home', 'HomeController@index');
 
 Route::resource('admin/users', 'AdminUsersController');
+
+Route::get('/admin', function () {
+
+    return view('admin.index');
+});
--- gulpfile.js
+++ gulpfile.js
@@ -11,30 +11,40 @@ var elixir = require("laravel-elixir");
  |
  */
 
-elixir(function(mix) {
+elixir(function (mix) {
   mix
     .sass("app.scss")
 
-    .styles(
-      [
-        "libs/blog-post.css",
-        "libs/bootstrap.css",
-        "libs/font-awesome.css",
-        "libs/metisMenu.css",
-        "libs/sb-admin-2"
-      ],
-      "./public/css/libs.css"
-    )
+    .styles([
 
-    .scripts(
-      [
-        "libs/bootstrap.js",
-        "libs/bootstrap.min.js",
-        "libs/jquery.js",
-        "libs/metisMenu.js",
-        "libs/sb-admin-2.js",
-        "libs/scripts.js"
-      ],
-      "./public/js/libs.js"
-    );
+      'libs/blog-post.css',
+      'libs/bootstrap.css',
+      'libs/font-awesome.css',
+      'libs/metisMenu.css',
+      'libs/sb-admin-2.css'
+
+
+    ], './public/css/libs.css')
+
+
+
+
+
+
+    .scripts([
+
+
+      'libs/jquery.js',
+      'libs/bootstrap.js',
+      'libs/metisMenu.js',
+      'libs/sb-admin-2.js',
+      'libs/scripts.js'
+
+
+
+
+
+
+    ], './public/js/libs.js')
+
 });
