2045d89df9557dec02b4ae3efd4682d7fc8d69a6
Ivan Mitic <ivanmitic91@gmail.com>
2019-04-14 11:39:12 +0200

    All media working

---
+++ app/Http/Controllers/AdminMediasController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Photo;
+
+use App\Http\Requests;
+use Illuminate\Http\Request;
+
+class AdminMediasController extends Controller
+{
+    public function index()
+    {
+
+        $photos = Photo::all();
+
+        return view('admin.media.index', compact('photos'));
+    }
+}
